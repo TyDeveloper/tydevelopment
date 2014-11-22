973b33f1e4e73b24dc77680af76ce47156c75060
Anthony AYME <anthony.ayme@fullsix.com>
2014-11-22 19:44:45 +0100

    Add rubys files

--- ruby/rubyapp/config/routes.rb
+++ ruby/rubyapp/config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
 
   resources :resources
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- ruby/rubyapp/db/schema.rb
+++ ruby/rubyapp/db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122151221) do
+ActiveRecord::Schema.define(version: 20141122172914) do
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
   create_table "models", force: true do |t|
     t.datetime "created_at"
